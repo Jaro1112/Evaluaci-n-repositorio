dc29c077c25c08e04e3a40a1891154bf24a56428
Jaro1112 <111395483+Jaro1112@users.noreply.github.com>
2022-11-13 16:39:47 -0500

    Add files via upload

---
+++ Ejercicio 4.7/punto47.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace trabajo4
+{
+
+    class clase
+    {
+
+        static public void Main()
+        {
+
+
+            // 4.7
+
+
+            int Numerouno;
+
+            int Numerodos;
+
+
+
+
+            Console.WriteLine("Escriba el numero mayor: ");
+            Numerouno = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Escriba el numero menor: ");
+            Numerodos = int.Parse(Console.ReadLine());
+
+            if (Numerouno % Numerodos == 0)
+            {
+
+                Console.WriteLine(Numerodos + " es divisor de " + Numerouno);
+
+            }
+            else
+            {
+
+                Console.WriteLine(Numerodos + " no es divisor de " + Numerouno);
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ Ejercicio 4.9/Punto49.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+namespace trabajo4
+{
+
+    class clase
+    {
+
+        static public void Main()
+        {
+
+
+            // 4.9
+
+            int N3;
+            int N4;
+            String SoR;
+            int Result;
+
+            Console.WriteLine("Escriba el primer numero: ");
+            N3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escriba el segundo numero: ");
+            N4 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Para suma Digite S para Resta Digite R ");
+            SoR = Console.ReadLine();
+
+            switch (SoR)
+            {
+                case "s":
+
+                    Result = N3 + N4;
+                    Console.WriteLine($"El resultado es: {Result}");
+                    break;
+
+                case "r":
+
+                    Result = N3 - N4;
+                    Console.WriteLine($"El resultado es: {Result}");
+                    break;
+
+                default:
+                    Console.WriteLine("No selecciono suma o resta");
+                    break;
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
