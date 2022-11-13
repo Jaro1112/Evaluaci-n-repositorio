95634ed8da3757e0a40b263823da2857edfa4ae6
Jaro1112 <111395483+Jaro1112@users.noreply.github.com>
2022-11-13 16:48:55 -0500

    Add files via upload

---
+++ Ejercicio 3.2/Parte32.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+
+
+namespace ejercicio32
+{
+
+   class clase
+    {
+
+        static void Main(String[] args)
+        {
+
+            
+            Console.WriteLine("ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero == 0)
+            {
+
+                Console.WriteLine("el numero"  + numero +   "es un numero neutro");
+
+
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("el numero"  + numero +   "es un numero positivo");
+            }
+            else
+            {
+                Console.WriteLine("el numero"  + numero +   "es un numero negativo");
+            }
+            Console.ReadKey();
+        }
+
+
+    }
