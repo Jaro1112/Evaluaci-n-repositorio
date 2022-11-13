7c2081d3d505c07d1adaa466b3fa46c36224c46c
Jaro1112 <111395483+Jaro1112@users.noreply.github.com>
2022-11-13 16:37:37 -0500

    Add files via upload

---
+++ Ejercicio 3.5/Parte35.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace trabajo
+{
+
+    class TrabajoRequisitos35
+    {
+
+        static void Main()
+        {
+            double Factura;
+
+            double descuento;
+
+            int Cantidadproduc;
+
+            double Precio;
+
+            double Preciobruto;
+
+            double Iva = 0.12;
+
+            double Ivacompra;
+
+            Console.WriteLine("Ingrese el precio del producto");
+            Precio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad que va a llevar");
+            Cantidadproduc = int.Parse(Console.ReadLine());
+
+            Ivacompra = (Precio * Cantidadproduc) * 0.12;
+
+            Console.WriteLine("El iva da un valor de: " + Ivacompra);
+
+            Preciobruto = (Precio * Cantidadproduc) + Ivacompra;
+
+            if (Preciobruto > 50000)
+            {
+
+                descuento = Preciobruto * 0.05;
+
+                Factura = Preciobruto - descuento;
+
+            }
+            else
+            {
+                Factura = Preciobruto;
+
+            }
+
+            Console.WriteLine("El precio de la factura es: " + Factura);
+
+
+
+
+
+        }
+    }
+
+}
